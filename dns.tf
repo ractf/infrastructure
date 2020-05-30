96e050a99b5afcca8c3e7b30003c343cb5ca643a
Daniel Milnes <thebeanogamer@gmail.com>
2020-05-30 21:07:37 +0100

    Add ractf.xyz

--- dns.tf
+++ dns.tf
@@ -2,6 +2,10 @@ resource "cloudflare_zone" "ractf-root-domain" {
     zone = "${vars.RACTF_DOMAIN}"
 }
 
+resource "cloudflare_zone" "ractf-shortener-domain" {
+    zone = "${vars.RACTF_SHORTENER_DOMAIN}"
+}
+
 resource "cloudflare_record" "homepage" {
   zone_id = "${cloudflare_zone.ractf-root-domain.id}"
   name    = "@"
@@ -22,6 +26,14 @@ resource "cloudflare_record" "2020" {
   zone_id = "${cloudflare_zone.ractf-root-domain.id}"
   name    = "2020"
   value   = "${aws_cloudfront_distribution.frontend_distribution.domain_name}"
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "homepage" {
+  zone_id = "${cloudflare_zone.ractf-shortener-domain.id}"
+  name    = "@"
+  value   = "${vars.RACTF_HOST}"
   type    = "A"
   ttl     = 3600
 }
