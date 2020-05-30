ee4d9c7e7a835db81c82489b1107e582814c9024
Daniel Milnes <thebeanogamer@gmail.com>
2020-05-30 19:48:53 +0100

    Define files DNS

--- dns.tf
+++ dns.tf
@@ -5,3 +5,12 @@ resource "cloudflare_record" "homepage" {
   type    = "A"
   ttl     = 3600
 }
+
+resource "cloudflare_record" "files" {
+  zone_id = "${vars.RACTF_ZONE_ID}"
+  name    = "files"
+  value   = "${aws_s3_bucket.frontend_bucket.website_endpoint}"
+  type    = "A"
+  ttl     = 3600
+}
+
