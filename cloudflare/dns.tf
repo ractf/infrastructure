130e1baabd7c8aedd7d03362255c9bba90e7a9a9
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-01 21:40:14 +0100

    Add the rest of DNS (#4)
    * Add API
    
    * Add www
    
    * Add mail
    
    * A -> CNAME
    
    * Fix typo
    
    * Correct address
    
    * Add the rest
    
    * Remove newline

--- cloudflare/dns.tf
+++ cloudflare/dns.tf
@@ -15,6 +15,24 @@ resource "cloudflare_record" "homepage" {
   proxied = true
 }
 
+resource "cloudflare_record" "www" {
+  zone_id = "${cloudflare_zone.ractf-root-domain.id}"
+  name    = "www"
+  value   = "${vars.RACTF_HOST}"
+  type    = "A"
+  ttl     = 3600
+  proxied = true
+}
+
+resource "cloudflare_record" "api" {
+  zone_id = "${cloudflare_zone.ractf-root-domain.id}"
+  name    = "api"
+  value   = "${vars.RACTF_HOST}"
+  type    = "A"
+  ttl     = 3600
+  proxied = true
+}
+
 resource "cloudflare_record" "files" {
   zone_id = "${cloudflare_zone.ractf-root-domain.id}"
   name    = "files"
@@ -28,9 +46,39 @@ resource "cloudflare_record" "frontend" {
   zone_id = "${cloudflare_zone.ractf-root-domain.id}"
   name    = "2020"
   value   = "${aws_cloudfront_distribution.frontend_distribution.domain_name}"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 3600
   proxied = false
+}
+
+resource "cloudflare_record" "mail" {
+  zone_id = "${cloudflare_zone.ractf-root-domain.id}"
+  value   = "${vars.RACTF_MAIL_HOST}"
+  type    = "MX"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "github" {
+  zone_id = "${cloudflare_zone.ractf-root-domain.id}"
+  name    = "_github-challenge"
+  value   = "${vars.GITHUB_TOKEN}"
+  type    = "TXT"
+  ttl     = 3600
+}
+
+
+resource "cloudflare_record" "spf" {
+  zone_id = "${cloudflare_zone.ractf-root-domain.id}"
+  value   = "v=spf1 a mx ip4:${vars.RACTF_MAIL_HOST} -all"
+  type    = "TXT"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "google-verify" {
+  zone_id = "${cloudflare_zone.ractf-root-domain.id}"
+  value   = "${vars.GOOGLE_TOKEN}"
+  type    = "TXT"
+  ttl     = 3600
 }
 
 resource "cloudflare_record" "shortener" {
