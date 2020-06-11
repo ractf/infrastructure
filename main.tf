f7d62852a77ac66ba8cc209cf8859e082e70e4dd
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-11 22:33:39 +0100

    The things I do for debugging

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-module "frontend" {
-    source = "./modules/aws/frontend"
-    deployment_name = var.RACTF_DOMAIN
-    providers = {
-        aws.cert = aws.cert
-    }
-}
-
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.deployment_name
   validation_method = "DNS"
