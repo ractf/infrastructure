1df1cd838bcd754e6d9483c60744f0032227cbe3
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-11 21:13:54 +0100

    Try and do variables better

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "RACTF_DOMAIN" {
+    type = string
+}
+
+variable "RACTF_SHORTENER_DOMAIN" {
+    type = string
+}
+
+variable "CLOUDFLARE_EMAIL" {
+    type = string
+}
+
+variable "CLOUDFLARE_API_KEY" {
+    type = string
+}
