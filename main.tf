b1527b649e4b331fc91aa6791a1b6883a653e708
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-11 22:34:58 +0100

    Remove testing

--- main.tf
+++ main.tf
@@ -1,13 +1,7 @@
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "asd"
-  validation_method = "DNS"
-  provider = aws.cert
-
-  tags = {
-    Environment = "production"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+module "frontend" {
+    source = "./modules/aws/frontend"
+    deployment_name = var.RACTF_DOMAIN
+    providers = {
+        aws.cert = aws.cert
+    }
 }
