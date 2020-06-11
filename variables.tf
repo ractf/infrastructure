6b676b5a9c3c7f5f21b9bf7c4126a632e9f7aa9f
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-11 21:58:44 +0100

    Define defaults

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "RACTF_DOMAIN" {
     type = string
+    default = "2020.ractf.co.uk"
 }
 
 variable "RACTF_SHORTENER_DOMAIN" {
     type = string
+    default = "ractf.xyz"
 }
 
 variable "CLOUDFLARE_EMAIL" {
