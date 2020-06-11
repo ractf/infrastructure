a06ac854d6e3f0dd62948f3b0cc0af67374c2dc9
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-11 22:32:47 +0100

    Test resource

--- main.tf
+++ main.tf
@@ -5,3 +5,17 @@ module "frontend" {
         aws.cert = aws.cert
     }
 }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.deployment_name
+  validation_method = "DNS"
+  provider = aws.cert
+
+  tags = {
+    Environment = "production"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
