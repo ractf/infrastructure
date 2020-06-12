ed696b194627915738ea8ae37a180bfb3b57753f
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-13 00:11:48 +0100

    Make the copy and paste less obvious

--- modules/aws/frontend/modules/deploy_user/outputs.tf
+++ modules/aws/frontend/modules/deploy_user/outputs.tf
@@ -1,11 +1,9 @@
 output "shell_deploy_id" {
-  type        = string
   value       = aws_iam_access_key.shell_deploy_key.id
   description = "The ID of the AWS deploy key for shell"
 }
 
 output "shell_deploy_key" {
-  type        = string
   value       = aws_iam_access_key.shell_deploy_key.secret
   description = "The key for the AWS deploy key for shell"
 }
--- modules/aws/frontend/outputs.tf
+++ modules/aws/frontend/outputs.tf
@@ -3,19 +3,16 @@ output "endpoint" {
 }
 
 output "s3_bucket" {
-  type        = string
   value       = aws_s3_bucket.frontend_bucket.id
   description = "The S3 bucket containing frontend"
 }
 
 output "shell_deploy_id" {
-  type        = string
   value       = aws_iam_access_key.shell_deploy_key.id
   description = "The ID of the AWS deploy key for shell"
 }
 
 output "shell_deploy_key" {
-  type        = string
   value       = aws_iam_access_key.shell_deploy_key.secret
   description = "The key for the AWS deploy key for shell"
 }
