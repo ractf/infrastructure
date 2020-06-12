f8db2a3042a0c3ab85851e756db8d085dbc33f2c
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-12 02:32:58 +0100

    Add static

--- main.tf
+++ main.tf
@@ -12,12 +12,17 @@ module "certificate" {
   }
 }
 
+module "static" {
+    source = "./modules/aws/static"
+    bucket_name = "files.${var.RACTF_DOMAIN}"
+}
+
 module "dns" {
   source            = "./modules/cloudflare/dns"
   domain            = var.RACTF_DOMAIN
   backend_endpoint  = var.ractf_host
   mail_endpoint     = var.mail_host
-  files_endpoint    = "COME BACK"
+  files_endpoint    = modules.static.bucket_endpoint
   frontend_endpoint = "COME BACK"
   github_token      = var.github_token
   google_token      = var.google_token
