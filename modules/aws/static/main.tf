2c4ba69221ee6a798b2fb88d563a24f54a9f1e46
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-14 15:58:20 +0100

    Restrict files access to Cloudflare

--- modules/aws/static/main.tf
+++ modules/aws/static/main.tf
@@ -1,11 +1,33 @@
 resource "aws_s3_bucket" "static_files" {
   bucket = var.bucket_name
-  acl    = "public-read"
 
   website {
     index_document = "index.html"
     error_document = "index.html"
   }
+}
+
+resource "aws_s3_bucket_policy" "static_files_cloudflare" {
+  bucket = "${aws_s3_bucket.static_files.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "files-cloudflare-only",
+  "Statement": [
+    {
+      "Sid": "CloudFlareAllow",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "${aws_s3_bucket.static_files.arn}/*",
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": ${data.cloudflare_ip_ranges.cloudflare.cidr_blocks}}
+      }
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_s3_bucket_object" "static_homepage" {
