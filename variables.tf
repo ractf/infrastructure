79f3223295e99bc5835c646302e36b8d80e46fb1
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-27 23:58:36 +0100

    Define the vars needed

--- variables.tf
+++ variables.tf
@@ -47,3 +47,23 @@ variable "deploy_account" {
   type = string
   description = "The ARN of the S3 deploy account"
 }
+
+variable "staging_endpoint" {
+  type = string
+  description = "The IP of the staging server"
+}
+
+variable "status_endpoint" {
+  type = string
+  description = "The DNS name of the statuspage"
+}
+
+variable "h1_token_production" {
+  type = string
+  description = "HackerOne verification token for Production"
+}
+
+variable "h1_token_staging" {
+  type = string
+  description = "HackerOne verification token for Staging"
+}
