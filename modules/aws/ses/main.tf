288933a5fe159bf911f62c4b4658d8d468ba16df
Daniel Milnes <thebeanogamer@gmail.com>
2020-06-28 01:52:52 +0100

    Create DKIM resource

--- modules/aws/ses/main.tf
+++ modules/aws/ses/main.tf
@@ -1,3 +1,7 @@
 resource "aws_ses_domain_identity" "ractf" {
   domain = var.domain
 }
+
+resource "aws_ses_domain_dkim" "ractf" {
+  domain = "${aws_ses_domain_identity.ractf.domain}"
+}
--- modules/aws/ses/outputs.tf
+++ modules/aws/ses/outputs.tf
@@ -2,3 +2,8 @@ output "domain_token" {
   description = "The domain verification token"
   value       = aws_ses_domain_identity.ractf.verification_token
 }
+
+output "dkim_records" {
+  description = "The DKIM records from SES"
+  value       = aws_ses_domain_dkim.ractf.dkim_tokens
+}
