d232d3d8325af53fa4c6ce1a8e8e0b96227c7f3c
Daniel Milnes <thebeanogamer@gmail.com>
2020-07-25 14:22:17 +0100

    Even more

--- modules/ractf/modules/frontend/modules/dns/main.tf
+++ modules/ractf/modules/frontend/modules/dns/main.tf
@@ -5,7 +5,7 @@ resource "cloudflare_zone" "ractf-root-domain" {
 resource "cloudflare_record" "frontend" {
   zone_id = cloudflare_zone.ractf-root-domain.id
   name    = var.deployment_name
-  value   = var.frontend_endpoint
+  value   = var.endpoint
   type    = "CNAME"
   proxied = false
 }
