7b1a283e4c74eed225c5ebc54ad84e2477fbc43c
Daniel Milnes <thebeanogamer@gmail.com>
2020-07-25 14:13:53 +0100

    Push your brain back in

--- modules/ractf/modules/static/main.tf
+++ modules/ractf/modules/static/main.tf
@@ -44,6 +44,6 @@ resource "aws_s3_bucket_object" "static_homepage" {
 module "dns" {
   source          = "./modules/dns"
   deployment_name = var.deployment_name
-  bucket_endpoint = aws_s3_bucket.static_files.bucket_endpoint
+  bucket_endpoint = aws_s3_bucket.static_files.website_endpoint
   root_domain     = var.root_domain
 }
