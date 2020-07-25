512de0bf66ba720396c6d4804ff6a4fdf6b3fa61
Daniel Milnes <thebeanogamer@gmail.com>
2020-07-25 14:02:43 +0100

    Rename some more variables

--- modules/ractf/modules/static/modules/dns/main.tf
+++ modules/ractf/modules/static/modules/dns/main.tf
@@ -5,7 +5,7 @@ resource "cloudflare_zone" "root-domain" {
 resource "cloudflare_record" "files" {
   zone_id = cloudflare_zone.root-domain.id
   name    = var.deployment_name
-  value   = var.files_endpoint
+  value   = var.bucket_endpoint
   type    = "CNAME"
   ttl     = 1
   proxied = true
