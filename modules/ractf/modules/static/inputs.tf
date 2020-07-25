edbbef6ab078cd5032f19f782e82f2f9db9a287e
Daniel Milnes <thebeanogamer@gmail.com>
2020-07-25 01:35:44 +0100

    Inputs

--- modules/ractf/modules/static/inputs.tf
+++ modules/ractf/modules/static/inputs.tf
@@ -2,3 +2,13 @@ variable "bucket_name" {
   type        = string
   description = "The name of the bucket for storing challenge files"
 }
+
+variable "root_domain" {
+  type = string
+  description = "The root domain for the RACTF deployment"
+}
+
+variable "deployment_name" {
+  type        = string
+  description = "The domain name to use when deploying the frontend"
+}
