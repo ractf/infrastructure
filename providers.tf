acccceea7d36f0f92c2c71492f8e3d58d5eff109
Daniel Milnes <thebeanogamer@gmail.com>
2020-07-25 15:17:05 +0100

    Terraform is trying to stop me doing silly things

--- providers.tf
+++ providers.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = var.aws_region
 }
 
+provider "aws" {
+  version = "~> 2.0"
+  alias   = "cert"
+  region  = "us-east-1"
+}
+
 provider "cloudflare" {
   version = "~> 2.0"
   email   = var.cloudflare_email
