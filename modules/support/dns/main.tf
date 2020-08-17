bfa1026e6f3ce080353dc518e56525fab86407d9
Daniel Milnes <daniel.milnes@lmax.com>
2020-08-17 21:41:16 +0100

    Add a load of conditionals

--- modules/support/dns/main.tf
+++ modules/support/dns/main.tf
@@ -13,6 +13,7 @@ resource "cloudflare_record" "home" {
 
 resource "cloudflare_record" "staging" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.staging_endpoint != "" ? 1 : 0
   name    = "staging"
   value   = var.staging_endpoint
   type    = "A"
@@ -22,6 +23,7 @@ resource "cloudflare_record" "staging" {
 
 resource "cloudflare_record" "staging-ephemeral" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.staging_endpoint != "" ? 1 : 0
   name    = "staging-ephemeral"
   value   = var.staging_endpoint
   type    = "A"
@@ -31,6 +33,7 @@ resource "cloudflare_record" "staging-ephemeral" {
 
 resource "cloudflare_record" "status" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.status_endpoint != "" ? 1 : 0
   name    = "status"
   value   = var.status_endpoint
   type    = "CNAME"
@@ -39,8 +42,8 @@ resource "cloudflare_record" "status" {
 }
 
 resource "cloudflare_record" "ses_dkim" {
-  count   = 3
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.ses_dkim_records != "" ? 3 : 0
   name    = "${element(var.ses_dkim_records, count.index)}._domainkey"
   value   = "${element(var.ses_dkim_records, count.index)}.dkim.amazonses.com"
   type    = "CNAME"
@@ -50,6 +53,7 @@ resource "cloudflare_record" "ses_dkim" {
 
 resource "cloudflare_record" "mail" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.mail_endpoint != "" ? 1 : 0
   name    = var.domain
   value   = var.mail_endpoint
   type    = "MX"
@@ -57,6 +61,7 @@ resource "cloudflare_record" "mail" {
 
 resource "cloudflare_record" "github" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.github_token != "" ? 1 : 0
   name    = "_github-challenge"
   value   = var.github_token
   type    = "TXT"
@@ -64,6 +69,7 @@ resource "cloudflare_record" "github" {
 
 resource "cloudflare_record" "github_secondary" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.github_token != "" ? 1 : 0
   name    = "_github-challenge-ractf"
   value   = var.github_token
   type    = "TXT"
@@ -71,6 +77,7 @@ resource "cloudflare_record" "github_secondary" {
 
 resource "cloudflare_record" "spf" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.mail_endpoint != "" ? 1 : 0
   name    = var.domain
   value   = "v=spf1 a mx a:${var.mail_endpoint} include:amazonses.com -all"
   type    = "TXT"
@@ -78,6 +85,7 @@ resource "cloudflare_record" "spf" {
 
 resource "cloudflare_record" "google-verify" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.google_token != "" ? 1 : 0
   name    = var.domain
   value   = var.google_token
   type    = "TXT"
@@ -85,6 +93,7 @@ resource "cloudflare_record" "google-verify" {
 
 resource "cloudflare_record" "production-h1" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.h1_token_production != "" ? 1 : 0
   name    = var.domain
   value   = var.h1_token_production
   type    = "TXT"
@@ -92,6 +101,7 @@ resource "cloudflare_record" "production-h1" {
 
 resource "cloudflare_record" "staging-h1" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.h1_token_staging != "" ? 1 : 0
   name    = "staging"
   value   = var.h1_token_staging
   type    = "TXT"
@@ -99,6 +109,7 @@ resource "cloudflare_record" "staging-h1" {
 
 resource "cloudflare_record" "ses-verify" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.ses_token != "" ? 1 : 0
   name    = var.domain
   value   = var.ses_token
   type    = "TXT"
@@ -106,6 +117,7 @@ resource "cloudflare_record" "ses-verify" {
 
 resource "cloudflare_record" "dkim" {
   zone_id = cloudflare_zone.ractf-root-domain.id
+  count = var.dkim_key != "" ? 1 : 0
   name    = "dkim._domainkey"
   value   = var.dkim_key
   type    = "TXT"
