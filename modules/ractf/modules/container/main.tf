b9a5e5a1b797456292c5edc3a7154d7afa429bac
Daniel Milnes <thebeanogamer@gmail.com>
2020-08-27 23:15:19 +0100

    Define a basic registry module

---
+++ modules/ractf/modules/container/main.tf
@@ -0,0 +1,30 @@
+resource "aws_ecr_repository" "registry" {
+  name                 = var.deployment_name
+  tags = {
+    Deployment = var.deployment_name
+  }
+}
+
+
+resource "aws_ecr_lifecycle_policy" "policy" {
+  repository = aws_ecr_repository.registry.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
