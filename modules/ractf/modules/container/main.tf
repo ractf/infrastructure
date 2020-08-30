b5825e778be131c3e58c65064f4a9ea4e451b4eb
Daniel Milnes <thebeanogamer@gmail.com>
2020-08-30 02:12:56 +0100

    Allow logging in to ECR

--- modules/ractf/modules/container/main.tf
+++ modules/ractf/modules/container/main.tf
@@ -100,3 +100,26 @@ resource "aws_ecr_repository_policy" "pull" {
   repository = aws_ecr_repository.registry.name
   policy     = data.aws_iam_policy_document.pull.json
 }
+
+data "aws_iam_policy_document" "login" {
+  statement {
+    sid = "LoginToECR"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_user.push.arn, aws_iam_user.pull.arn]
+    }
+  }
+}
+
+resource "aws_iam_policy" "login" {
+  name        = "${var.deployment_name}EcrLogin"
+  path        = "/registry"
+  description = "Allow logging in to ECR"
+
+  policy = data.aws_iam_policy_document.login.json
