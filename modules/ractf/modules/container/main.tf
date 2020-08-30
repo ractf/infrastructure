5a5e0b90297dcb09afa8f131c0122443f96e0da1
Daniel Milnes <thebeanogamer@gmail.com>
2020-08-30 01:49:33 +0100

    tagStatus=ANY must have the lowest priority

--- modules/ractf/modules/container/main.tf
+++ modules/ractf/modules/container/main.tf
@@ -11,34 +11,34 @@ resource "aws_ecr_lifecycle_policy" "policy" {
 
   policy = <<EOF
 {
-    "rules": [
-        {
-          "rulePriority": 1,
-          "description": "Expire images older than 14 days",
-          "selection": {
-            "tagStatus": "any",
-            "countType": "sinceImagePushed",
-            "countUnit": "days",
-            "countNumber": 14
-          },
-          "action": {
-            "type": "expire"
-          }
-        },
-        {
-          "rulePriority": 2,
-          "description": "Expire untagged images older than 1 day",
-          "selection": {
-            "tagStatus": "untagged",
-            "countType": "sinceImagePushed",
-            "countUnit": "days",
-            "countNumber": 1
-          },
-          "action": {
-            "type": "expire"
-          }
-        }
-    ]
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire untagged images older than 1 day",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Expire images older than 14 days",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 14
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
 }
 EOF
 }
