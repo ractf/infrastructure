3140e7f84c1775a250e286d61285dcdb0e5ab7b2
Daniel Milnes <thebeanogamer@gmail.com>
2020-12-28 18:43:52 +0000

    Expose the Traefik API through Consul This is an example we'll swap out later

--- modules/support/consul/main.tf
+++ modules/support/consul/main.tf
@@ -8,3 +8,23 @@ resource "consul_config_entry" "proxy_defaults" {
     }
   })
 }
+
+resource "consul_config_entry" "production_gateway" {
+  kind = "ingress-gateway"
+  name = "production-gateway"
+  config_json = jsonencode({
+    Listeners = [{
+      Port    = 80
+      Protocl = "TCP"
+      Services = [{
+        Name = "traefik-api"
+      }]
+    }]
+  })
+}
+
+resource "consul_intention" "production_gateway" {
+  source_name      = "production-gateway"
+  destination_name = "traefik-api"
+  action           = "allow"
+}
