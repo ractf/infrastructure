550d293ea51e563e48ee0be2c0bc2d52e1d31106
Daniel Milnes <thebeanogamer@gmail.com>
2020-12-30 18:19:38 +0000

    Revert "Define a testing gateway (#137)"
    This reverts commit ee04521a5843b54c292f937f1f17b5f9aac80c61.

--- modules/support/consul/main.tf
+++ modules/support/consul/main.tf
@@ -21,7 +21,7 @@ resource "consul_config_entry" "production_gateway" {
       Port     = 80
       Protocol = "tcp"
       Services = [{
-        Name = "traefik-api"
+        Name = "nginx"
       }]
     }]
   })
@@ -29,7 +29,7 @@ resource "consul_config_entry" "production_gateway" {
 
 resource "consul_intention" "production_gateway" {
   source_name      = "production-gateway"
-  destination_name = "traefik-api"
+  destination_name = "nginx"
   action           = "allow"
 }
 
@@ -57,54 +57,4 @@ resource "consul_acl_policy" "production_gateway" {
 resource "consul_acl_token" "production_gateway" {
   description = "Consul Connect gateway"
   policies = [consul_acl_policy.production_gateway.name]
-}
-
-resource "consul_config_entry" "testing_gateway" {
-  kind = "ingress-gateway"
-  name = "testing-gateway"
-  config_json = jsonencode({
-    TLS = {
-      Enabled = false
-    }
-
-    Listeners  = [{
-      Port     = 80
-      Protocol = "tcp"
-      Services = [{
-        Name = "nginx"
-      }]
-    }]
-  })
-}
-
-resource "consul_intention" "testing_gateway" {
-  source_name      = "testing-gateway"
-  destination_name = "nginx"
-  action           = "allow"
-}
-
-resource "consul_acl_policy" "testing_gateway" {
-  name  = "testing-gateway"
-  rules = <<-RULE
-    service "testing-gateway" {
-      policy = "write"
-    }
-
-    service_prefix "" {
-      policy = "read"
-    }
-
-    node_prefix "" {
-      policy = "read"
-    }
-
-    agent_prefix "ix2gateway" {
-      policy = "write"
-    }
-    RULE
-}
-
-resource "consul_acl_token" "testing_gateway" {
-  description = "Consul Connect gateway"
-  policies = [consul_acl_policy.testing_gateway.name]
 }
