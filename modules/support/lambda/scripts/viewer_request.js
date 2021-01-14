7b399259181a30bf3f7259d71e55a3370a3aa011
Daniel Milnes <thebeanogamer@gmail.com>
2021-01-14 23:52:09 +0000

    Don't use a variable that doesn't exist (#152)

--- modules/support/lambda/scripts/viewer_request.js
+++ modules/support/lambda/scripts/viewer_request.js
@@ -1,5 +1,6 @@
 'use strict';
 exports.handler = (event, context, callback) => {
-    request.uri = event.Records[0].cf.request.uri.replace(/\/$/, '\/index.html');
+    var request = event.Records[0].cf.request;
+    request.uri = request.uri.replace(/\/$/, '\/index.html');
     return callback(null, request);
 };
