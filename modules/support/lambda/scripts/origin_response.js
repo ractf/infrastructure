484b3904a0166ed9f8941abaeaf555a70fac9534
Daniel Milnes <daniel.milnes@lmax.com>
2021-01-15 08:59:04 +0000

    Disable CSP Botters is wrong, this CSP does break things

--- modules/support/lambda/scripts/origin_response.js
+++ modules/support/lambda/scripts/origin_response.js
@@ -10,10 +10,10 @@ exports.handler = (event, context, callback) => {
         key: 'Strict-Transport-Security',
         value: 'max-age=63072000; includeSubdomains; preload'
     }];
-    headers['content-security-policy'] = [{
-        key: 'Content-Security-Policy',
-        value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
-    }];
+    //headers['content-security-policy'] = [{
+    //    key: 'Content-Security-Policy',
+    //    value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+    //}];
     headers['x-content-type-options'] = [{
         key: 'X-Content-Type-Options',
         value: 'nosniff'
