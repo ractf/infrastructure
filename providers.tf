eb9917b2061fd908557de944239ffa5f920d2b8c
Daniel Milnes <thebeanogamer@gmail.com>
2021-02-20 01:50:37 +0000

    Terraform 0.14 migration (#167)

--- providers.tf
+++ providers.tf
@@ -1,16 +1,13 @@
 provider "aws" {
-  version = "~> 3.2"
   region  = var.aws_region
 }
 
 provider "aws" {
-  version = "~> 3.2"
   alias   = "cert"
   region  = "us-east-1"
 }
 
 provider "cloudflare" {
-  version = "~> 2.0"
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
 }
