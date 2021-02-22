a60dbc5b86e31b0f3a34998c67341b6782cc6e0c
Daniel Milnes <thebeanogamer@gmail.com>
2021-02-22 21:41:16 +0000

    Revert "Terraform 0.14 migration (#167)"
    This reverts commit eb9917b2061fd908557de944239ffa5f920d2b8c.

--- providers.tf
+++ providers.tf
@@ -1,13 +1,16 @@
 provider "aws" {
+  version = "~> 3.2"
   region  = var.aws_region
 }
 
 provider "aws" {
+  version = "~> 3.2"
   alias   = "cert"
   region  = "us-east-1"
 }
 
 provider "cloudflare" {
+  version = "~> 2.0"
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
 }
