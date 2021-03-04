16ed8df7c760e4fe97481a48fa082c7d4bcad342
Daniel Milnes <thebeanogamer@gmail.com>
2021-03-04 14:39:23 +0000

    Destroy all frontends This is so the next PR can re-create them with Brotli

--- modules/ractf/modules/frontend/main.tf
+++ modules/ractf/modules/frontend/main.tf
@@ -78,83 +78,83 @@ resource "aws_cloudfront_cache_policy" "cache_policy" {
   }
 }
 
-resource "aws_cloudfront_distribution" "frontend_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
+#resource "aws_cloudfront_distribution" "frontend_distribution" {
+#  origin {
+#    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+#    origin_id   = local.s3_origin_id
+#
+#    s3_origin_config {
+#      origin_access_identity = aws_cloudfront_origin_access_identity.frontend_distribution.cloudfront_access_identity_path
+#    }
+#  }
+#
+#  enabled             = true
+#  is_ipv6_enabled     = true
+#  comment             = var.deployment_name
+#  default_root_object = "index.html"
+#
+#  aliases = ["${var.deployment_name}.${var.root_domain}"]
+#
+#  default_cache_behavior {
+#    allowed_methods  = ["GET", "HEAD"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id = local.s3_origin_id
+#    cache_policy_id  = aws_cloudfront_cache_policy.cache_policy.id
+#
+#    viewer_protocol_policy = "redirect-to-https"
+#    min_ttl                = 0
+#    default_ttl            = 3600
+#    max_ttl                = 86400
+#    compress               = true
+#
+#    lambda_function_association {
+#      event_type   = "origin-response"
+#      lambda_arn   = var.origin_response_arn
+#      include_body = false
+#    }
+#
+#    dynamic "lambda_function_association" {
+#      for_each = local.react_array
+#      content {
+#        event_type   = "viewer-request"
+#        lambda_arn   = var.viewer_request_arn
+#        include_body = false
+#      }
+#    }
+#  }
+#
+#  restrictions {
+#    geo_restriction {
+#      restriction_type = "none"
+#    }
+#  }
+#
+#  tags = {
+#    Environment = "production"
+#    Deployment  = local.nice_deployment_name
+#  }
+#
+#  viewer_certificate {
+#    acm_certificate_arn      = module.certificate.arn
+#    ssl_support_method       = "sni-only"
+#    minimum_protocol_version = "TLSv1.2_2019"
+#  }
+#
+#  custom_error_response {
+#    error_caching_min_ttl = 0
+#    error_code            = "404"
+#    response_code         = var.react ? "200" : "404"
+#    response_page_path    = var.error_page
+#  }
+#}
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.frontend_distribution.cloudfront_access_identity_path
-    }
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = var.deployment_name
-  default_root_object = "index.html"
-
-  aliases = ["${var.deployment_name}.${var.root_domain}"]
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-    cache_policy_id  = aws_cloudfront_cache_policy.cache_policy.id
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-
-    lambda_function_association {
-      event_type   = "origin-response"
-      lambda_arn   = var.origin_response_arn
-      include_body = false
-    }
-
-    dynamic "lambda_function_association" {
-      for_each = local.react_array
-      content {
-        event_type   = "viewer-request"
-        lambda_arn   = var.viewer_request_arn
-        include_body = false
-      }
-    }
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  tags = {
-    Environment = "production"
-    Deployment  = local.nice_deployment_name
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = module.certificate.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2019"
-  }
-
-  custom_error_response {
-    error_caching_min_ttl = 0
-    error_code            = "404"
-    response_code         = var.react ? "200" : "404"
-    response_page_path    = var.error_page
-  }
-}
-
-resource "cloudflare_record" "frontend" {
-  zone_id = var.zone
-  name    = var.deployment_name
-  value   = aws_cloudfront_distribution.frontend_distribution.domain_name
-  type    = "CNAME"
-  proxied = false
-}
+#resource "cloudflare_record" "frontend" {
+#  zone_id = var.zone
+#  name    = var.deployment_name
+#  value   = aws_cloudfront_distribution.frontend_distribution.domain_name
+#  type    = "CNAME"
+#  proxied = false
+#}
 
 module "certificate" {
   source          = "./modules/certificate"
