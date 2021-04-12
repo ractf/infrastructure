b3f7a8b453a2884772214f75278ae32e715fb79e
Daniel Milnes <thebeanogamer@gmail.com>
2021-04-13 00:35:11 +0100

    Try and setup staging

---
+++ modules/support/staging/inputs.tf
@@ -0,0 +1,9 @@
+variable "staging_host" {
+  type        = string
+  description = "The host running RACTF Staging"
+}
+
+variable "zone" {
+  type        = string
+  description = "The Cloudflare Zone ID"
+}
