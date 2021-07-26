35ed699b473fc667b18f743b1f12407a76500afa
Daniel Milnes <thebeanogamer@gmail.com>
2021-07-26 21:42:22 +0100

    Turn Cloudflare Proxy On (#210)
    * Turn Cloudflare Proxy On
    This should fix the problems from #158 by adding a pagerule
    
    * Fix priority

--- modules/ractf/modules/backend/main.tf
+++ modules/ractf/modules/backend/main.tf
@@ -4,7 +4,7 @@ resource "cloudflare_record" "api" {
   value   = var.backend_endpoint
   type    = "A"
   ttl     = 1
-  proxied = false
+  proxied = true
 }
 
 resource "newrelic_synthetics_monitor" "stats" {
