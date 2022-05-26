756cb7dfe339806f0ed09422b6756d474ce3b593
Daniel Milnes <daniel.milnes@lmax.com>
2022-05-26 08:27:48 +0100

    Remove New Relic

--- modules/ractf/modules/backend/main.tf
+++ modules/ractf/modules/backend/main.tf
@@ -6,21 +6,3 @@ resource "cloudflare_record" "api" {
   ttl     = 1
   proxied = true
 }
-
-resource "newrelic_synthetics_monitor" "stats" {
-  name              = "${var.deployment_name}.${var.root_domain} Stats"
-  type              = "SIMPLE"
-  frequency         = 1
-  status            = "ENABLED"
-  locations         = ["AWS_EU_WEST_2"]
-  uri               = "https://api-${var.deployment_name}.${var.root_domain}/api/v2/stats/stats/"
-  validation_string = "{\"s\":true,\"m\":\"\",\"d\":"
-  verify_ssl        = true
-  depends_on        = [cloudflare_record.api]
-}
-
-resource "newrelic_synthetics_alert_condition" "stats" {
-  policy_id  = var.new_relic_policy_id
-  name       = "${var.deployment_name}.${var.root_domain} Alert Condition"
-  monitor_id = newrelic_synthetics_monitor.stats.id
-}
