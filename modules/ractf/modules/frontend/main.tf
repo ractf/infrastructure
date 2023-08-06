e57ce650b71844394685cff5133e7171d0030a75
Daniel Milnes <daniel@daniel-milnes.uk>
2023-08-06 14:26:21 +0100

    Allow the deploy user to get object ACLs (#227)

--- modules/ractf/modules/frontend/main.tf
+++ modules/ractf/modules/frontend/main.tf
@@ -23,6 +23,7 @@ data "aws_iam_policy_document" "frontend_distribution" {
   statement {
     actions = [
       "s3:GetObject",
+      "s3:GetObjectAcl",
       "s3:PutObject",
       "s3:PutObjectAcl",
       "s3:DeleteObject"
